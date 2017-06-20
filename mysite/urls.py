e6a9361a89c5759819f79f4dde754d1224d37230
samuel <samuel.shahinaj@gmail.com>
2017-06-20 17:39:07 +0200

    aggiunti livelli di sicurezza

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views
 
 urlpatterns = [
     # Examples:
@@ -8,5 +9,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-	url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls')),
 ]
